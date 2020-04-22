5cbe019329fd6a3905eba503a5e48cf1890157b6
Hung Nguyen <hungnv1099@gmail.com>
2020-04-22 16:44:29 +0700

    add print html.

--- src/screens/more/printHtml/PrintHtml.js
+++ src/screens/more/printHtml/PrintHtml.js
@@ -1,45 +1,99 @@
-import React from 'react';
-import { Image, View, StyleSheet, Button, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react';
+import { Image, View, StyleSheet, Button, Text, TouchableOpacity, ScrollView, TextInput } from 'react-native';
 import ToolBarPrintHtml from '../../../components/toolbar/ToolBarPrintHtml';
 import { Images, Colors } from '../../../theme';
+import { WebView } from 'react-native-webview';
+import htmlDefault from '../../../data/html/htmlDefault';
+import useDidMountEffect from '../../../customHook/useDidMountEffect';
+import dialogManager from '../../../components/dialog/DialogManager';
+import { HTTPService } from '../../../data/services/HttpService';
+import { ApiPath } from '../../../data/services/ApiPath';
 
 export default (props) => {
+
+    const [tabType, setTabType] = useState(1);
+
     return (
         <View style={{ flex: 1 }}>
             <ToolBarPrintHtml
                 navigation={props.navigation} title="Print HTML"
-                clickDefault={() => { alert("clickDefault") }}
-                clickLoadOnline={() => { alert("clickLoadOnline") }}
-                clickShow={() => { alert("clickShow") }}
+                clickDefault={() => { setTabType(1) }}
+                clickLoadOnline={() => { setTabType(2) }}
+                clickShow={() => { setTabType(3) }}
             />
-
+            {
+                tabType == 1 ?
+                    <DefaultComponent />
+                    : (tabType == 2 ? <OnlineComponent /> : <WebviewComponent />)
+            }
         </View>
     );
 };
 
-const HeaderComponent = () => {
+const DefaultComponent = () => {
+    const [contentHtml, setContentHtml] = useState(htmlDefault);
     return (
-        <View style={{ backgroundColor: Colors.colorchinh, justifyContent: "space-between", flexDirection: "row", alignItems: "center", padding: 20 }}>
-            <View >
-                <View style={{ flexDirection: "row", alignItems: "center" }}>
-                    <Image source={Images.icon_person} style={[{ width: 50, height: 50, marginRight: 20 }]} />
-                    <Text style={{ marginTop: 10 }}>An NB</Text>
-                </View>
-                <Text style={{ marginTop: 15 }}>Chi nhánh</Text>
-            </View>
-            <Text style={{ textDecorationLine: "underline" }}>Đăng xuất</Text>
-        </View>
+        <ScrollView style={{ flex: 1 }}>
+            <TextInput style={{
+                margin: 10,
+                flex: 1,
+            }} multiline={true} onChangeText={text => {
+                setContentHtml(text)
+            }} value={contentHtml} />
+        </ScrollView>
+    )
+}
 
+const OnlineComponent = () => {
+
+    const [dataHTML, setDataHTML] = useState("");
+    const onClickLoadOnline = useCallback(() => {
+        dialogManager.showLoading();
+        let params = {};
+        new HTTPService().setPath(ApiPath.PRINT_TEMPLATES).GET(params).then((res) => {
+            console.log("onClickLoadOnline res ", res);
+            setDataHTML(res.Content)
+            dialogManager.hiddenLoading()
+        }).catch((e) => {
+            console.log("onClickLoadOnline err ", e);
+            dialogManager.hiddenLoading()
+        })
+    }, [])
+
+    useEffect(() => {
+        onClickLoadOnline()
+    }, [])
+
+    return (
+        <ScrollView style={{ flex: 1 }}>
+            <TextInput style={{
+                margin: 10,
+                flex: 1,
+            }} multiline={true} onChangeText={text => {
+                setDataHTML(text)
+            }} value={dataHTML} />
+        </ScrollView>
     )
 }
 
 const WebviewComponent = () => {
+
+    useEffect(() => {
+        dialogManager.showLoading();
+    }, [])
+
     return (
-        <View style={{ padding: 20, borderBottomWidth: 0.5, borderBottomColor: "#ddd" }}>
-            <Text style={{ color: Colors.colorchinh, fontSize: 18 }}>Print setup</Text>
-            <TouchableOpacity onPress={() => alert("okÏ")}>
-                <Text style={{ marginTop: 20 }}>HTML print</Text>
-            </TouchableOpacity>
+        <View style={{ flex: 1 }}>
+            <WebView
+                source={{ uri: 'https://reactnative.dev/docs/webview.html' }}
+                style={{ marginTop: 0 }}
+                onError={syntheticEvent => {
+                    dialogManager.hiddenLoading();
+                }}
+                onLoadEnd={syntheticEvent => {
+                    dialogManager.hiddenLoading();
+                }}
+            />
         </View>
     )
 }
